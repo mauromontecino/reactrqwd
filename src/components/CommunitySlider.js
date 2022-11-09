e0a98713c892482a6b8c35eba4001c0f931c6905
mauromontecino <mauromontecino2@gmail.com>
2022-11-09 14:36:24 -0300

    .

--- src/components/CommunitySlider.js
+++ src/components/CommunitySlider.js
@@ -27,7 +27,7 @@ const CommunitySlider = ({ testimonials }) => {
                 <img src={image} alt="" />
               </div>
               {/* message & name */}
-              <div className="absolute bottom-[30px] p-[20px] text-white text-center">
+              <div className="absolute bottom-[30px] p-[20px] text-white text-center resenia">
                 <div className="mb-8 italic text-lg font-light">{message}</div>
                 <div className="flex items-center justify-center gap-x-[3px]">
                   <span className="text-[30px] text-primary-200 font-bold">
