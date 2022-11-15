6407a6c3ccc1b94fec51e1377f70383777e9b5cc
mauromontecino <mauromontecino2@gmail.com>
2022-11-15 20:58:36 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,17 @@ const Footer = () => {
   // destructure footer data
   const { logo, copyrightText } = footer;
   return (
-    <footer className="bg-neutral-500 h-[125px] md:h-[195px] px-[20px]">
+    <footer className="bg-neutral-500 h-[350px] md:h-[195px] px-[20px]">
+      <div className="text-center">
+        <br />
+        <h2>Teléfono:</h2>
+        <p>+54 9 299 5175495</p>
+        <br />
+        <h2>Dirección:</h2>
+        <p>Río Negro 51, Centenario, Neuquén</p>
+      </div>
       <div
-        className="container mx-auto h-full flex justify-between items-center md:items-end md:pb-[50px]"
+        className="container mx-auto h-full flex justify-between items-center md:pb-[200px]"
         data-aos="fade-up"
         data-aos-delay="200"
       >
